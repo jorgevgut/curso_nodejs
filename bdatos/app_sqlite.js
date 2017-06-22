03b995af9e32db02c2513b822bdff898b155b0d6
luiko <lokoluis_box@hotmail.com>
2017-06-21 21:42:30 -0600

    mejorando la legibilidad de la promesa

--- bdatos/app_sqlite.js
+++ bdatos/app_sqlite.js
@@ -4,19 +4,10 @@ var driver = require('sqlite');
 var promesa = driver.open('./base.sqlite');
 
 // then, catch
-promesa.then((driver) => {
-    driver.exec('CREATE TABLE IF NOT EXISTS usuarios (nombre TEXT, edad INT);')
-        .then((driver) => {
-            driver.exec('INSERT INTO usuarios VALUES("Jorge", 26);')
-                .then((driver) => {
-                    driver.get('SELECT * FROM usuarios')
-                        .then((resultado) => {
-                            console.log(resultado);
-                        })
-                })
-        }).catch((causa) => {
-            console.log(causa);
-        })
-});
-
+promesa
+    .then(driver => driver.exec('CREATE TABLE IF NOT EXISTS usuarios (nombre TEXT, edad INT);'))
+    .then(driver => driver.exec('INSERT INTO usuarios VALUES("Jorge", 26);'))
+    .then(driver => driver.get('SELECT * FROM usuarios'))
+    .then(console.log)
+    .catch(console.error);
 // operaciones
